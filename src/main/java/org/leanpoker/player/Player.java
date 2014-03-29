8aebc3e714eee4ea91f23dbd70f2b579012852a4
katona.zsolt <katona.zsolt.mail@gmail.com>
2014-03-29 10:40:19 +0100

    basic betting

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,8 +7,18 @@ public class Player {
     static final String VERSION = "Korda Gyuri Bácsi pókerrobotja";
 
     public static int betRequest(JsonElement request) {
-        return 0;
-    }
+		try {
+			if (Math.random() < 0.1) {
+				return 0;
+			} else {
+				return 1;
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			return 0;
+		}
+	}
 
     public static void showdown(JsonElement game) {
     }
