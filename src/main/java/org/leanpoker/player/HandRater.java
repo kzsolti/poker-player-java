3e33c7d156fdbd8a4dfd12a285d820f568e4d2c7
unknown <foobarkk@gmail.com>
2014-03-29 15:40:51 +0100

    const added to handrater

--- src/main/java/org/leanpoker/player/HandRater.java
+++ src/main/java/org/leanpoker/player/HandRater.java
@@ -5,4 +5,14 @@ package org.leanpoker.player;
  */
 public abstract class HandRater {
      public abstract float rate(Hand hand);
+    public static int HIGH_CARD = 100;
+    public static int ONE_PAIR = 200;
+    public static int TWO_PAIRS = 300;
+    public static int THREE_OF_A_KIND = 400;
+    public static int STRAIGHT = 500;
+    public static int FLUSH = 600;
+    public static int FULL_HOUSE = 700;
+    public static int FOUR_OF_A_KIND = 800;
+    public static int STRAIGHT = 900;
+    public static int ROYAL_FLUSH = 1000;
 }
---
+++ src/main/java/org/leanpoker/player/HandRaterStraight.java
@@ -0,0 +1,18 @@
+package org.leanpoker.player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by kiss.katalin on 2014.03.29..
+ */
+public class HandRaterStraight extends HandRater {
+    @Override
+    public float rate(Hand hand) {
+
+        List<Integer> bela = new ArrayList<Integer>();
+
+
+        return 0;
+    }
+}
