7af4bd910f21f65c52eac079d1310c07c049fa63
katona.zsolt <katona.zsolt.mail@gmail.com>
2014-03-29 09:46:05 +0100

    robot név

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -2,11 +2,9 @@ package org.leanpoker.player;
 
 import com.google.gson.JsonElement;
 
-import java.util.Map;
-
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "Korda Gyuri Bácsi pókerrobotja";
 
     public static int betRequest(JsonElement request) {
         return 0;
