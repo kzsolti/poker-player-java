1b0b54e0cc8a421f7226dcea83151c43e6607845
unknown <foobarkk@gmail.com>
2014-03-29 16:17:32 +0100

    added new rater only class

---
+++ src/main/java/org/leanpoker/player/HandRaterHighCard.java
@@ -0,0 +1,43 @@
+package org.leanpoker.player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by fulop on 2014.03.29..
+ */
+public class HandRaterHighCard extends HandRater {
+    @Override
+    public float rate(Hand hand) {
+        List<Card> cards = hand.getCards();
+        if (cards.size()<5) {
+            return 0;
+        }
+        Map<String, Integer> suits = new HashMap<>();
+        for (Card card : cards) {
+            addSuiteToMap(suits, card.getSuit() );
+                     }
+
+        for (Integer numSuits : suits.values()) {
+            if (numSuits >= 5) {
+                return HandRater.FLUSH;
+            }
+        }
+
+        return 0;
+    }
+
+    public void addSuiteToMap(Map<String, Integer> suits, String suit) {
+        Integer num = suits.get(suit);
+        if (num == null) {
+            suits.put(suit, new Integer(1));
+        } else {
+            System.out.println(num);
+            num = new Integer(num.intValue() + 1);
+            System.out.println(num);
+            suits.put(suit, num);
+        }
+
+    }
+}
