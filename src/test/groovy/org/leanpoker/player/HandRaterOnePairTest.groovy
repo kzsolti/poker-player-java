5ff2be27750a0c1e21d34a0146eb0f3686aea10e
katona.zsolt <katona.zsolt.mail@gmail.com>
2014-03-29 15:48:09 +0100

    rename

--- src/test/groovy/org/leanpoker/player/HandRaterOnePairTest.groovy
+++ src/test/groovy/org/leanpoker/player/HandRaterOnePairTest.groovy
@@ -5,14 +5,14 @@ import spock.lang.Specification
 /**
  * Created by katona.zsolt on 2014.03.29..
  */
-class OnePairRaterTest extends Specification {
+class HandRaterOnePairTest extends Specification {
 
 	def "has one pair"() {
 		given:
 		def hand = new Hand()
 		hand.addCard(new Card('5', 'diamonds'))
 		hand.addCard(new Card('5', 'spades'))
-		def onePairRater = new OnePairRater()
+		def onePairRater = new HandRaterOnePair()
 
 		expect:
 		onePairRater.rate(hand) > 0
